3ec843ea9bba7a6878db4c68e59b527ec98a6fe7
SIM SEONGHYEON <131232777+U-Gosu@users.noreply.github.com>
2023-05-17 15:18:46 +0900

    [Bronze V] Title: A/B, Time: 224 ms, Memory: 17988 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        double a = scn.nextDouble();
+        double b = scn.nextDouble();
+        System.out.println(a / b);
+    }
+}
