0e32fcbfc431af5de86d8f8176ff86fef4fb8046
SIM SEONGHYEON <131232777+U-Gosu@users.noreply.github.com>
2023-05-17 15:46:27 +0900

    [Bronze III] Title: 최소, 최대, Time: 1328 ms, Memory: 110964 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+
+        int[] arr = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        bw.write(arr[0] + " " + arr[arr.length - 1]);
+
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
+}
+
