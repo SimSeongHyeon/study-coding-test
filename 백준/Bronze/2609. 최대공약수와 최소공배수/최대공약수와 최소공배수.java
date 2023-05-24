e8496c9855328b011cf1d88f77c489f5abb4a70e
SIM SEONGHYEON <mrshsim@gmail.com>
2023-05-24 15:57:37 +0900

    [Bronze I] Title: 최대공약수와 최소공배수, Time: 152 ms, Memory: 16124 KB -BaekjoonHub

--- 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
+++ 백준/Bronze/2609. 최대공약수와 최소공배수/최대공약수와 최소공배수.java
@@ -10,30 +10,21 @@ public class Main {
 
         int a = Integer.parseInt(st.nextToken());
         int b = Integer.parseInt(st.nextToken());
-        int gcd = 1;
-        int lcm = a * b;
 
-        if (a >= b) {
-            for (int i = 2; i <= a; i++) {
-                if (a % i == 0 && b % i == 0) gcd = i;
-            }
-        }
-
-        else {
-            for (int i = 2; i <= b; i++){
-                if (a % i == 0 && b % i == 0) gcd = i;
-            }
-        }
-
-        for (int j = lcm; j >= 1; j--) {
-            if (j % a == 0 && j % b == 0) lcm = j;
-        }
-
-        bw.write(gcd +"\n" + lcm);
+        bw.write(gcd(a, b) + "\n" + a * b / gcd(a, b));
 
         br.close();
 
         bw.flush();
         bw.close();
+    }
+
+    static int gcd (int a, int b) {
+        while (b != 0) {
+            int r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
     }
 }
