14c5e50394d07d7a8b1ecaa1fe10767acead30c4
SIM SEONGHYEON <mrshsim@gmail.com>
2023-05-27 22:44:57 +0900

    [Bronze II] Title: Hashing, Time: 124 ms, Memory: 14260 KB -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int L = Integer.parseInt(br.readLine());
+        String str = br.readLine();
+        long answer = 0;
+        int pow = 1;
+
+        for (int i = 0; i < L; i++) {
+            answer += (long)(str.charAt(i) - 'a' + 1) * pow;
+            pow = pow * 31 % 1234567891;
+        }
+
+        bw.write(answer % 1234567891 + "");
+
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
+}
