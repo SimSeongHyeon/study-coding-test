7527ed5aa0000a4c5d835fa33d7393ba2d953dff
SIM SEONGHYEON <mrshsim@gmail.com>
2023-05-30 12:27:23 +0900

    [Silver V] Title: 너의 평점은, Time: 132 ms, Memory: 14688 KB -BaekjoonHub

--- 백준/Silver/25206. 너의 평점은/너의 평점은.java
+++ 백준/Silver/25206. 너의 평점은/너의 평점은.java
@@ -10,8 +10,8 @@ public class Main {
 
         double creditSum = 0;
         double gradeSum = 0;
-        double gradePoint = 0;
-        double avg = 0.;
+        double gradePoint;
+        double avg;
 
         for (int i = 0; i < 20; i++) {
             st = new StringTokenizer(br.readLine());
@@ -22,15 +22,7 @@ public class Main {
 
             double scr = 0;
 
-            if (grade.equals("A+")) gradePoint = 4.5;
-            else if (grade.equals("A0")) gradePoint = 4.0;
-            else if (grade.equals("B+")) gradePoint = 3.5;
-            else if (grade.equals("B0")) gradePoint = 3.0;
-            else if (grade.equals("C+")) gradePoint = 2.5;
-            else if (grade.equals("C0")) gradePoint = 2.0;
-            else if (grade.equals("D+")) gradePoint = 1.5;
-            else if (grade.equals("D0")) gradePoint = 1.0;
-            else if (grade.equals("F")) gradePoint = 0.0;
+            gradePoint = gradePoint(grade);
 
             if (!grade.equals("P")) {
                 creditSum = creditSum + credit;
@@ -38,6 +30,7 @@ public class Main {
             }
 
             gradeSum = gradeSum + scr;
+
         }
 
         avg = gradeSum / creditSum;
@@ -48,5 +41,21 @@ public class Main {
 
         bw.flush();
         bw.close();
+    }
+
+    static double gradePoint(String grade) {
+        double gradePoint = 0;
+
+        if (grade.equals("A+")) gradePoint = 4.5;
+        else if (grade.equals("A0")) gradePoint = 4.0;
+        else if (grade.equals("B+")) gradePoint = 3.5;
+        else if (grade.equals("B0")) gradePoint = 3.0;
+        else if (grade.equals("C+")) gradePoint = 2.5;
+        else if (grade.equals("C0")) gradePoint = 2.0;
+        else if (grade.equals("D+")) gradePoint = 1.5;
+        else if (grade.equals("D0")) gradePoint = 1.0;
+        else if (grade.equals("F")) gradePoint = 0.0;
+
+        return gradePoint;
     }
 }
