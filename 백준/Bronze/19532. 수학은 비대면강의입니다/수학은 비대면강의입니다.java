04f075ab548789c2f390e9d9f9b8e334c391c90d
SIM SEONGHYEON <mrshsim@gmail.com>
2023-06-02 21:25:42 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 232 ms, Memory: 19608 KB -BaekjoonHub

---
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+        int e = Integer.parseInt(st.nextToken());
+        int f = Integer.parseInt(st.nextToken());
+        
+        for (int x = -999; x < 1000; x++) {
+            for (int y = -999; y < 1000; y++) {
+                if (a * x + b * y == c && d * x + e * y == f) {
+                    bw.write(x + " " + y);
+                }
+            }
+        }
+
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
+}
