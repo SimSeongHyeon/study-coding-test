d9680856c640ae49211bf0f1a97a09060d959ec6
SIM SEONGHYEON <mrshsim@gmail.com>
2023-06-06 20:30:23 +0900

    [Bronze V] Title: 팩토리얼, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int fac = 1;
+
+        if (N > 0) {
+            for (int i = 1; i <= N; i++) {
+                fac = fac * i;
+            }
+        }
+
+        bw.write(fac + "");
+
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
+}
