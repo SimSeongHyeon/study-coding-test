a8e9b330961d505dbc583e00e90a63b2346f2d0a
SIM SEONGHYEON <mrshsim@gmail.com>
2023-06-25 22:12:45 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 820 ms, Memory: 94480 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -1,10 +1,7 @@
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
@@ -14,14 +11,13 @@ public class Main {
         for (int i = 0; i < N; i++) {
             arr[Integer.parseInt(br.readLine()) + 1000000] = true;
         }
-
+        
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i]) bw.write(i - 1000000 + "\n");
+            if (arr[i]) sb.append(i - 1000000).append("\n");
         }
 
-        br.close();
+        System.out.println(sb);
 
-        bw.flush();
-        bw.close();
     }
 }
