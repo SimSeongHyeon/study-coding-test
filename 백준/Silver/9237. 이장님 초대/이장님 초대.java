4610a86194788a59b45b09dacce99543fc4f3b7e
SIM SEONGHYEON <mrshsim@gmail.com>
2023-07-05 11:20:29 +0900

    [Silver V] Title: 이장님 초대, Time: 640 ms, Memory: 30656 KB -BaekjoonHub

---
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        Integer[] grow = new Integer[N];
+
+        int day = 1;
+        int visit = 0;
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        for (int i = 0; i < N; i++) {
+            grow[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(grow, Collections.reverseOrder());
+
+        for (int i = 0; i < N; i++) {
+            grow[i] += day;
+            day++;
+        }
+
+        Arrays.sort(grow, Collections.reverseOrder());
+
+        System.out.println(grow[0] + 1);
+    }
+}
