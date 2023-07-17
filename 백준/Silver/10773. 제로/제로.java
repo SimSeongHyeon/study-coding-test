e5b96da281fdacb5d38b087117140bf8870c9172
SIM SEONGHYEON <mrshsim@gmail.com>
2023-07-18 02:20:00 +0900

    [Silver IV] Title: 제로, Time: 212 ms, Memory: 22680 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        /* 스택 클래스 사용
+
         Stack<Integer> stack = new Stack<>();
 
         int K = Integer.parseInt(br.readLine());
@@ -26,5 +28,34 @@ public class Main {
         System.out.println(sum);
 
         br.close();
+
+        */
+
+        int K = Integer.parseInt(br.readLine());
+
+        int index = 0;
+
+        int[] arr = new int[K];
+
+        for (int i = 0; i < K; i++) {
+            int n = Integer.parseInt(br.readLine());
+
+            if (n == 0) {
+                index--;
+            }
+
+            else {
+                arr[index] = n;
+                index++;
+            }
+        }
+
+        int sum = 0;
+
+        for (int i = 0; i < index; i++) {
+            sum += arr[i];
+        }
+
+        System.out.println(sum);
     }
 }
