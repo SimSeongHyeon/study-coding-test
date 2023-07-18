267867c2d1cb22ff622e7d48c5bbc6b7ce704d1b
SIM SEONGHYEON <mrshsim@gmail.com>
2023-07-19 00:10:03 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 124 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.java
@@ -1,20 +1,20 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.IOException;
- 
+import java.io.*;
+
 public class Main {
- 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
- 
-		int num = Integer.parseInt(br.readLine());
-		int count = 0;
- 
-		while (num >= 5) {
-			count += num / 5;
-			num /= 5;
-		}
-		System.out.println(count);
-	}
- 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+
+        for (int i = 1; i <= N; i++) {
+            int num = i;
+            while (num % 5 == 0) {
+                cnt++;
+                num /= 5;
+            }
+        }
+
+        System.out.println(cnt);
+    }
 }
