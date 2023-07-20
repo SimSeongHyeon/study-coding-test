b28c2a962820367a1cf04fa924ff02f6ebf53502
SIM SEONGHYEON <mrshsim@gmail.com>
2023-07-21 01:33:06 +0900

    [Bronze I] Title: 세로읽기, Time: 124 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Bronze/10798. 세로읽기/세로읽기.java
+++ 백준/Bronze/10798. 세로읽기/세로읽기.java
@@ -1,26 +1,32 @@
-import java.util.Scanner;
+import java.io.*;
+import java.rmi.activation.ActivationGroup_Stub;
 
 public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-	public static void main(String[] args) {
-		Scanner scan = new Scanner(System.in);
-		char[][] cWord = new char[5][15];
-		int max = 0;	// 입력받는 문자열 중 가장 긴 문자열의 길이
+        Character[][] characters = new Character[5][15];
 
-		for(int i=0; i<5; i++) {
-			String str = scan.next();
-			max = Math.max(max, str.length());	// 가장 긴 문자열 체크
-			for(int j=0; j<str.length(); j++)
-				cWord[i][j] = str.charAt(j);
-		}
+        int max = 0;
 
-		for(int i=0; i<max; i++) {
-			for(int j=0; j<5; j++) {
-				if(cWord[j][i] == '\0')	// char 배열의 초깃값 = '\0'
-					continue;
-				System.out.print(cWord[j][i]);
-			}
-		}
-		scan.close();
-	}
+        for (int i = 0; i < 5; i++) {
+            String str = br.readLine();
+
+            max = Math.max(max, str.length());
+
+            for (int j = 0; j < str.length(); j++) {
+                characters[i][j] = str.charAt(j);
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < max; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (characters[j][i] != null) sb.append(characters[j][i]);
+            }
+        }
+
+        System.out.println(sb);
+    }
 }
