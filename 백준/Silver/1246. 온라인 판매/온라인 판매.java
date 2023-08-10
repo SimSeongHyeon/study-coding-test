9b0c81390530c383bfc0a87d4dcf7450cf32c5ff
SIM SEONGHYEON <mrshsim@gmail.com>
2023-08-10 23:39:45 +0900

    [Silver IV] Title: 온라인 판매, Time: 172 ms, Memory: 16160 KB -BaekjoonHub

---
+++ 백준/Silver/1246. 온라인 판매/온라인 판매.java
@@ -0,0 +1,43 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] cost = new int[M];
+
+        for (int i = 0; i < M; i++) {
+            cost[i] = Integer.parseInt(br.readLine());
+        }
+
+        int benefit = 0;
+
+        Arrays.sort(cost);
+
+        int[] sum = new int[M];
+
+        for (int i = 0; i < M; i++) {
+                sum[i] = cost[i] * Math.min(N, M - i);
+        }
+
+        int price = 0;
+
+        for (int i = 0; i < M; i++) {
+            if (sum[i] > benefit) {
+                benefit = sum[i];
+                price = i;
+            }
+        }
+
+        System.out.println(cost[price] + " " + benefit);
+    }
+}
