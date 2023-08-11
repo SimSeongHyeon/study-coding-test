b4d6469f3f9447ffc3e7f084cc15e1a9f98227d9
SIM SEONGHYEON <mrshsim@gmail.com>
2023-08-11 17:18:20 +0900

    [Silver IV] Title: 좋은 단어, Time: 356 ms, Memory: 22108 KB -BaekjoonHub

---
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            cnt += isGood(br.readLine());
+        }
+
+        System.out.println(cnt);
+    }
+
+    static int isGood (String str) {
+        Stack<Character> strings = new Stack<>();
+
+        strings.push(str.charAt(0));
+
+        for (int i = 1; i < str.length(); i++) {
+            if (strings.size() > 0 && strings.peek() == str.charAt(i)) strings.pop();
+
+            else if (strings.empty() || (strings.peek() != str.charAt(i))) strings.push(str.charAt(i));
+        }
+
+        if (strings.empty()) return 1;
+        else return 0;
+    }
+}
