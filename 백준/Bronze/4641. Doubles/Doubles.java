9947447118418fd5c4d6fecd035fee07a21f8ff1
SIM SEONGHYEON <mrshsim@gmail.com>
2023-08-15 00:05:39 +0900

    [Bronze I] Title: Doubles, Time: 124 ms, Memory: 14008 KB -BaekjoonHub

--- 백준/Bronze/4641. Doubles/Doubles.java
+++ 백준/Bronze/4641. Doubles/Doubles.java
@@ -1,38 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        StringTokenizer st;
-
         while (true) {
+            boolean[] exist = new boolean[100];
+
+            String[] str = br.readLine().split(" ");
+            if (str.length < 2) break;
+
+            for (String s : str) {
+                exist[Integer.parseInt(s)] = true;
+            }
+
             int cnt = 0;
-            int i = 0;
-            int[] num = new int[16];
 
-            st = new StringTokenizer(br.readLine());
-
-            while (st.hasMoreTokens()) {
-                int n = Integer.parseInt(st.nextToken());
-                if (n == -1) return;
-                num[i] = n;
-                i++;
+            for (int i = 1; i < 50; i++) {
+                if (exist[i] && exist[i * 2]) cnt++;
             }
-
-            Arrays.sort(num);
-
-            for (int j = 1; j < 15; j++) {
-                for (int k = j + 1; k < 16; k++) {
-                    if(num[j] == 0) continue;
-                    if (num[j] * 2 == num[k]) cnt++;
-                }
-            }
-
             System.out.println(cnt);
         }
     }
