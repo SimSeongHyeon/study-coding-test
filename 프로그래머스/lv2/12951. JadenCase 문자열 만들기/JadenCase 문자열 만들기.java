838cedcf01521f96425b15c10e5888c91d1303df
SIM SEONGHYEON <mrshsim@gmail.com>
2023-08-17 23:44:14 +0900

    [level 2] Title: JadenCase 문자열 만들기, Time: 2.83 ms, Memory: 72.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        
+        String[] words = s.split(" ");
+        
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].length() == 0) answer += " ";
+
+            else {
+                answer += words[i].substring(0, 1).toUpperCase();
+                answer += words[i].substring(1, words[i].length()).toLowerCase();
+                answer += " ";
+            }
+        }
+        
+        if (s.charAt(s.length() - 1) == ' ') return answer;
+        return answer.substring(0, answer.length() - 1);
+    }
+}
