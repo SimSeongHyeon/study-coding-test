8a81d90c6f74853801f75e07d3f236d55b1c9f93
SIM SEONGHYEON <mrshsim@gmail.com>
2023-08-18 14:13:54 +0900

    [level 2] Title: 올바른 괄호, Time: 19.88 ms, Memory: 52.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        
+        Stack<Character> stack = new Stack<>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') stack.push(s.charAt(i));
+            else if (s.charAt(i) == ')' && stack.empty()) return false;
+            else stack.pop();
+        }
+        
+        if (!stack.empty()) answer = false;
+
+        return answer;
+    }
+}
