ea374d428a1b78e85abd7f901624e1db23ac674e
SIM SEONGHYEON <mrshsim@gmail.com>
2023-08-18 14:06:08 +0900

    [level 2] Title: 최솟값 만들기, Time: 1.90 ms, Memory: 52 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12941. 최솟값 만들기/최솟값 만들기.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+
+class Solution
+{
+    public int solution(int []A, int []B)
+    {
+        int answer = 0;
+
+        Arrays.sort(A);
+        Arrays.sort(B);
+        
+        for (int i = 0; i < A.length; i++) {
+            answer += A[A.length - 1 - i] * B[i];
+        }
+        
+        int min = Integer.MAX_VALUE;
+
+        return answer;
+    }
+}
