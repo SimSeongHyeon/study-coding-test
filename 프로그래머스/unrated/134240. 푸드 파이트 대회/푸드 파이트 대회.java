411d8df6da3c3ec2a577f1a561fbf8933431b1a7
SIM SEONGHYEON <mrshsim@gmail.com>
2023-09-12 09:36:26 +0900

    [unrated] Title: 푸드 파이트 대회, Time: 2.01 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/134240. 푸드 파이트 대회/푸드 파이트 대회.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(int[] food) {
+        String answer = "";
+        
+        for (int i = 1; i < food.length; i++) {
+            for (int j = 0; j < food[i] / 2; j++) {
+                answer += String.valueOf(i);
+            }
+        }
+        
+        StringBuffer sb = new StringBuffer(answer);
+    
+        answer += "0";
+
+        answer += sb.reverse().toString();
+        
+        return answer;
+    }
+}
