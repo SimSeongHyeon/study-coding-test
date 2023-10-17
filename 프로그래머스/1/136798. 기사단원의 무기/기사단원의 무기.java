11b9ccaadadf2bf41327df5527d776790ee45bf4
SIM SEONGHYEON <mrshsim@gmail.com>
2023-10-17 09:30:36 +0900

    [level 1] Title: 기사단원의 무기, Time: 0.20 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -1,17 +1,19 @@
 class Solution {
+
     public int solution(int number, int limit, int power) {
-        int answer = 0;
-        
+        int[] count = new int[number + 1];    
         for (int i = 1; i <= number; i++) {
-            int div = 0;
-            
-            for (int j = 1; j * j <= i; j++) {
-                if (j * j == i) div++;
-                else if (i % j == 0) div += 2;
+            for (int j = 1; j <= number / i; j++) {
+                count[i * j]++;
             }
-            
-            if (div <= limit) answer += div;
-            else answer += power;
+        }
+        int answer = 0;
+        for (int i = 1; i <= number; i++) {
+            if (count[i] > limit) {
+                answer += power;
+            } else {
+                answer += count[i];
+            }
         }
         return answer;
     }
