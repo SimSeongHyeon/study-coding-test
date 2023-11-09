b2d9ec24ea48d9ceb8ff22ddf75e6b439229afb4
SIM SEONGHYEON <mrshsim@gmail.com>
2023-11-09 14:30:02 +0900

    [Bronze II] Title: 나머지, Time: 124 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -7,13 +7,11 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         boolean[] mod = new boolean[42];
-        int[] num = new int[10];
         int cnt = 0;
 
         for(int i = 0; i < 10; i++) {
             int a = Integer.parseInt(br.readLine());
-            num[i] = a;
-            mod[num[i] % 42] = true;
+            mod[a % 42] = true;
         }
 
         for (int j = 0; j < mod.length; j++) {
