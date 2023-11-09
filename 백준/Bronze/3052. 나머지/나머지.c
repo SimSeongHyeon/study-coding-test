bd867401712efc8b61824263a49c46d38f988402
SIM SEONGHYEON <mrshsim@gmail.com>
2023-11-09 14:37:16 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.c
+++ 백준/Bronze/3052. 나머지/나머지.c
@@ -1,23 +1,20 @@
-#include <stdio.h>
+#include <stdio.h> 
 
 int main() {
-    int nums[1001] = {0}; // 배열 크기를 1001로 설정
-    int answer = 0;
+	int nums[42] ={0};
+	int answer = 0;
 
-    for (int i = 0; i < 10; i++) {
-        int n;
-        scanf("%d", &n);
-        
-        if (n >= 0 && n <= 1000) { // 입력 범위 확인
-            nums[n % 42] = 1;
-        }
-    }
+	for (int i = 0; i < 10; i++) {
+		int n;
+		scanf("%d", &n);
+		nums[n % 42] = 1;
+	}
 
-    for (int i = 0; i < 42; i++) { // 42로 나누기
-        if (nums[i] == 1) answer++;
-    }
+	for (int i = 0; i < 42; i++) {
+		if (nums[i] == 1) answer++;
+	}
 
-    printf("%d", answer);
-
+	printf("%d", answer);
+    
     return 0;
 }
