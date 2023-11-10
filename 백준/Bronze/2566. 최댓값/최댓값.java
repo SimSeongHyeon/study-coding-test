1d05d26a399743953c007e14c00c66cd81503a91
SIM SEONGHYEON <mrshsim@gmail.com>
2023-11-10 13:16:29 +0900

    [Bronze III] Title: 최댓값, Time: 144 ms, Memory: 15988 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.java
@@ -0,0 +1,41 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int board[][] = new int[9][9];
+        int max = 0;
+        int row = 0;
+        int col = 0;
+
+        for (int i = 0; i < 9; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 9; j++) {
+                board[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        for (int[] n : board) {
+            for (int m : n) {
+                max = Math.max(max, m);
+            }
+        }
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == max) {
+                    row = i + 1;
+                    col = j + 1;
+                }
+            }
+        }
+
+        System.out.println(max);
+        System.out.println(row + " " + col);
+    }
+}
