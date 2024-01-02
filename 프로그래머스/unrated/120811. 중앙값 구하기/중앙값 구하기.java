645fbfff55fa4005998c8de27e3ee4a2dae9ab89
SIM SEONGHYEON <mrshsim@gmail.com>
2024-01-02 18:04:36 +0900

    [level unrated] Title: 중앙값 구하기, Time: 0.34 ms, Memory: 73.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120811. 중앙값 구하기/중앙값 구하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        int len = array.length / 2;
+        
+        Arrays.sort(array);
+        
+        answer = array[len];
+        
+        return answer;
+    }
+}
