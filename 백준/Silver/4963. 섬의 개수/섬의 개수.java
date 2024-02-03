04229d944ad59526ae71413d2733a0f3150aa177
SIM SEONGHYEON <mrshsim@gmail.com>
2024-02-03 18:58:25 +0900

    [Silver II] Title: 섬의 개수, Time: 168 ms, Memory: 15616 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,76 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int w, h, islandCount;
+    static int[] dr = {0, 1, 1, 1, 0, -1, -1, -1};
+    static int[] dc = {1, 1, 0, -1, -1, -1, 0, 1};
+    static int[][] map;
+    static boolean[][] visited;
+
+    static void dfs(int x, int y) {
+        visited[x][y] = true;
+
+        for (int i = 0; i < 8; i++) {
+            int nx = x + dr[i];
+            int ny = y + dc[i];
+
+            if (nx < 0 || ny <0 || nx >= h || ny >= w) {
+                continue;
+            }
+
+            if (!visited[nx][ny] && map[nx][ny] == 1) {
+                dfs(nx, ny);
+            }
+        }
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+
+        StringBuilder sb = new StringBuilder();
+
+        while (true) {
+            st = new StringTokenizer(br.readLine());
+
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+
+            if (w == 0 && h == 0) {
+                break;
+            }
+
+            map = new int[h][w];
+            visited = new boolean[h][w];
+
+            islandCount = 0;
+
+            for (int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine());
+
+                for (int j = 0; j < w; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (!visited[i][j] && map[i][j] == 1) {
+                        dfs(i, j);
+                        islandCount++;
+                    }
+                }
+            }
+
+            sb.append(islandCount).append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
