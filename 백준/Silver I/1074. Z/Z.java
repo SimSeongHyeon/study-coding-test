673b5ebe6211d71016fec968c6a37df015828cd1
SIM SEONGHYEON <mrshsim@gmail.com>
2024-02-15 13:33:58 +0900

    [Silver I] Title: Z, Time: 76 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Silver I/1074. Z/Z.java
+++ 백준/Silver I/1074. Z/Z.java
@@ -16,50 +16,23 @@ public class Main {
 
         int result = 0;
 
-        System.out.println(z(N, r, c, result));
-    }
+        while (N > 0) {
+            int half = (int) Math.pow(2, N - 1);
 
-    static int z(int N, int r, int c, int result) {
-        if (N == 0) {
-            return result;
+            if (r < half && c >= half) {
+                c -= half;
+                result += (int) Math.pow(4, N - 1);
+            } else if (r >= half && c < half) {
+                r -= half;
+                result += (int) Math.pow(4, N - 1) * 2;
+            } else if (r >= half && c >= half) {
+                r -= half;
+                c -= half;
+                result += (int) Math.pow(4, N - 1) * 3;
+            }
+            N--;
         }
 
-        int half = (int) Math.pow(2, N - 1);
-
-        //1, 2, 3, 4사분면(2사분면은 좌표 조정이 필요 없다.)
-        if (r < half && c >= half) {
-            c -= half;
-
-            result += (int) Math.pow(4, N - 1);
-
-            N--;
-
-            return z(N, r, c, result);
-
-        } else if (r < half) {
-            N--;
-
-            return z(N, r, c, result);
-
-        } else if (c < half) {
-            r -= half;
-
-            result += (int) Math.pow(4, N - 1) * 2;
-
-            N--;
-
-            return z(N, r, c, result);
-
-        } else {
-            r -= half;
-
-            c -= half;
-
-            result += (int) Math.pow(4, N - 1) * 3;
-
-            N--;
-
-            return z(N, r, c, result);
-        }
+        System.out.println(result);
     }
 }
