d4ab52992a3ba66016e2f9d30686f4544ce4b4b1
SIM SEONGHYEON <mrshsim@gmail.com>
2024-03-13 23:44:37 +0900

    [level 0] Title: 문자열 붙여서 출력하기, Time: 157.50 ms, Memory: 72.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181946. 문자열 붙여서 출력하기/문자열 붙여서 출력하기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        String answer = "";
+        
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        
+        answer = a + b;
+        System.out.println(answer);
+    }
+}
