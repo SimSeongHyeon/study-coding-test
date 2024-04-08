7486203d89d40123308ac28bd779e76ae1fdb7d8
SIM SEONGHYEON <mrshsim@gmail.com>
2024-04-08 23:57:29 +0900

    [level 0] Title: 특정한 문자를 대문자로 바꾸기, Time: 0.06 ms, Memory: 80 MB -BaekjoonHub

---
+++ 프로그래머스/0/181873. 특정한 문자를 대문자로 바꾸기/특정한 문자를 대문자로 바꾸기.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String my_string, String alp) {
+        StringBuilder answer = new StringBuilder();
+        
+        for (int i = 0; i < my_string.length(); i++) {
+            if (String.valueOf(my_string.charAt(i)).equals(alp)) {
+                answer.append(String.valueOf(my_string.charAt(i)).toUpperCase());
+            } else {
+                answer.append(String.valueOf(my_string.charAt(i)));
+            }
+        }
+        return String.valueOf(answer);
+    }
+}
