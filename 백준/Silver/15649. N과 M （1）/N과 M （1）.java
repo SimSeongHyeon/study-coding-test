35445b14f4e2c9431080cfd838560c550d1bdc9c
SIM SEONGHYEON <mrshsim@gmail.com>
2024-12-25 06:25:55 +0900

    [Silver III] Title: N과 M (1), Time: 836 ms, Memory: 55000 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -3,7 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Main {
+public class Main{
+    
+    static int[] numbers;
+    static boolean[] isVisited;
+    
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
@@ -12,36 +16,45 @@ public class Main {
         int N = Integer.parseInt(stringTokenizer.nextToken());
         int M = Integer.parseInt(stringTokenizer.nextToken());
 
-        int[] sequence = new int[M];
-        boolean[] visited = new boolean[N];
-
-        backTracking(N, M, sequence, visited, 0);
-
+        getCombination(N, M);
     }
 
-    static void backTracking (int N, int M, int[] sequence, boolean[] isVisited, int depth) {
-        if (depth == sequence.length) {
-            StringBuilder stringBuilder = new StringBuilder();
+    static void getCombination(int N, int M) {
+        numbers = new int[M];
+        isVisited = new boolean[N + 1];
 
-            for (int n : sequence) {
-                stringBuilder.append(n).append(" ");
-            }
+        backtracking(N, M, numbers, isVisited, 0);
+    }
 
-            System.out.println(stringBuilder.toString().trim());
+    static void backtracking(int N, int M, int[] numbers, boolean[] isVisited, int depth) {
+        if (depth == M) {
+            printCombination(numbers);
 
             return;
         }
 
-        for (int i = 0; i < N; i++) {
+        for (int i = 1; i <= N; i++) {
             if (!isVisited[i]) {
                 isVisited[i] = true;
 
-                sequence[depth] = i + 1;
+                numbers[depth] = i;
 
-                backTracking(N, M, sequence, isVisited, depth + 1);
+                backtracking(N, M, numbers, isVisited, depth + 1);
 
                 isVisited[i] = false;
             }
         }
+
     }
+
+    static void printCombination(int[] numbers) {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int number : numbers) {
+            stringBuilder.append(number).append(" ");
+        }
+
+        System.out.println(stringBuilder.toString().trim());
+    }
+
 }
